f8c38c76800b368e5a95de5c6987a4e1550964cf
Rostik_S <95083201+frizeone@users.noreply.github.com>
2023-12-15 19:28:25 +0300

    Contyme work to sequrite

--- src/main/java/com/example/springdatabasicdemo/AppSecurityConfiguration.java
+++ src/main/java/com/example/springdatabasicdemo/AppSecurityConfiguration.java
@@ -1,7 +1,14 @@
 package com.example.springdatabasicdemo;
 
 import com.example.springdatabasicdemo.repositories.UserRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
 @Configuration
 public class AppSecurityConfiguration {
@@ -10,5 +17,19 @@ public class AppSecurityConfiguration {
 
     public AppSecurityConfiguration(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new DelegatingSecurityContextRepository(
+                new RequestAttributeSecurityContextRepository(),
+                new HttpSessionSecurityContextRepository()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
